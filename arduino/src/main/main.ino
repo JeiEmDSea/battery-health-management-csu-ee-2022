8bc2f1783f81013936e3570762af9970d419d626
John Mark D. Centeno <jeiem.centeno@gmail.com>
2023-01-05 14:20:07 +0800

    Added init lines for sensors

--- arduino/src/main/main.ino
+++ arduino/src/main/main.ino
@@ -1,15 +1,28 @@
+#include "Arduino.h"
 #include "LiquidCrystal_I2C.h"
 #include "DS3231.h"
 #include "SdFat.h"
 #include "SimpleRelay.h"
 #include "Voltmeter.h"
+#include "PreciseLM35.h"
 
+// ? PIN DEFINITIONS
+#define VOLT_SENSOR_PIN A0
+#define CELL_1_TEMP_SENSOR_PIN A2
+#define CELL_2_TEMP_SENSOR_PIN A3
+#define CELL_3_TEMP_SENSOR_PIN A4
+
+// ? I/O & CALIBRATION VALUES
+unsigned int voltSensorMaxVoltage = 25;
+unsigned int voltSensorLowerBound = 50;
+unsigned int voltSensorUpperBound = 1023;
+
+// ? OBJECTS
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 DS3231 rtc(SDA, SCL);
 SdFat sdCard;
 SdFile sdFile;
-
-//? RELAY VARIALBLES
+// ? relay channels
 SimpleRelay cell1Negative = SimpleRelay(2, true);
 SimpleRelay cell1Postive = SimpleRelay(3, true);
 SimpleRelay cell2Negative = SimpleRelay(4, true);
@@ -17,19 +30,26 @@ SimpleRelay cell2Postive = SimpleRelay(5, true);
 SimpleRelay cell3Negative = SimpleRelay(6, true);
 SimpleRelay cell3Postive = SimpleRelay(7, true);
 SimpleRelay load = SimpleRelay(8, true);
+// ? sensors
+Voltmeter voltSensor(VOLT_SENSOR_PIN, voltSensorMaxVoltage, voltSensorLowerBound, voltSensorUpperBound);
+PreciseLM35 cell1TempSensor(CELL_1_TEMP_SENSOR_PIN);
+PreciseLM35 cell2TempSensor(CELL_2_TEMP_SENSOR_PIN);
+PreciseLM35 cell3TempSensor(CELL_3_TEMP_SENSOR_PIN);
 
 void setup()
 {
   Serial.begin(9600);
   initLCD();
   initSDcard();
+  voltSensor.initialize();
   rtc.begin();
   turnOffRelayChannels();
 }
 
 void loop()
 {
-  // ? put your main code here, to run repeatedly:
+    // ? put your main code here, to run repeatedly
+  delaySeconds(1);
 }
 
 void initLCD()
@@ -69,10 +89,10 @@ void turnOffRelayChannels()
 void testCell(int cell)
 {
   connectCell(cell);
-  //! record values
+  // ! record values
   load.on();
   delay(150);
-  //! record values
+  // ! record values
   delay(150);
   load.off();
   disconnectCell(cell);
